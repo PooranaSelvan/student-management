052e2d45e117994ab23a5fb8d4f7e159c13248ca
PooranaSelvan <pooranaselvan67@gmail.com>
2024-10-27 15:34:36 +0530

    Rolled Back

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,13 @@
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const connectDB = async () => {
     try {
-        const client = new MongoClient(process.env.MONGO_URI);
-
-        await client.connect();
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        
         console.log("Connected To The Database");
-
-        // Optionally, return the client for further operations
-        return client;
     } catch (err) {
         console.error(`Error - ${err.message}`);
         process.exit(1);
