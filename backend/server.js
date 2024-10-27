355b24ad52e7b9b1a144964c544b6bedf2c1c504
PooranaSelvan <pooranaselvan67@gmail.com>
2024-10-27 16:57:40 +0530

    Changed Something Again

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+     origin: ['https://student-management-ecru.vercel.app/'],
+     methods: 'GET,POST,PUT,DELETE,PATCH,HEAD'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
